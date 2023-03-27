727515f5d55556c8260de71182e3002c384f679b
Rafael Weber Becker <rafael.becker@aegro.com.br>
2023-03-27 17:14:44 -0300

    adicionar colisão no eixo Y

--- src/collisions.cpp
+++ src/collisions.cpp
@@ -4,15 +4,19 @@ namespace Collisions
 {
   bool boundingBoxesCollision(glm::vec4 firstObjPosition, glm::vec3 firstObjBboxMin, glm::vec3 firstObjBboxMax, glm::vec4 secondObjPosition, glm::vec3 secondObjBboxMin, glm::vec3 secondObjBboxMax)
   {
-    // coordenadas do bounding box do primeiro objeto
+    // coordenadas do bounding box do primeiro objetdo
     float bboxFirstObjMinX = (firstObjPosition.x + firstObjBboxMin.x);
     float bboxFirstObjMaxX = (firstObjPosition.x + firstObjBboxMax.x);
+    float bboxFirstObjMinY = (firstObjPosition.y + firstObjBboxMin.y);
+    float bboxFirstObjMaxY = (firstObjPosition.y + firstObjBboxMax.y);
     float bboxFirstObjMinZ = (firstObjPosition.z + firstObjBboxMin.z);
     float bboxFirstObjMaxZ = (firstObjPosition.z + firstObjBboxMax.z);
 
     // coordenadas do bounding box do segundo objeto
     float bboxSecondObjMinX = (secondObjPosition.x + secondObjBboxMin.x);
     float bboxSecondObjMaxX = (secondObjPosition.x + secondObjBboxMax.x);
+    float bboxSecondObjMinY = (secondObjPosition.y + secondObjBboxMin.y);
+    float bboxSecondObjMaxY = (secondObjPosition.y + secondObjBboxMax.y);
     float bboxSecondObjMinZ = (secondObjPosition.z + secondObjBboxMin.z);
     float bboxSecondObjMaxZ = (secondObjPosition.z + secondObjBboxMax.z);
 
@@ -21,6 +25,15 @@ namespace Collisions
           bboxFirstObjMinX <= bboxSecondObjMinX && bboxFirstObjMinX >= bboxSecondObjMaxX ||
           bboxFirstObjMaxX >= bboxSecondObjMinX && bboxFirstObjMaxX <= bboxSecondObjMaxX ||
           bboxFirstObjMaxX <= bboxSecondObjMinX && bboxFirstObjMaxX >= bboxSecondObjMaxX))
+    {
+      return false;
+    }
+
+    // se o Y não se intercepta
+    if (!(bboxFirstObjMinY >= bboxSecondObjMinY && bboxFirstObjMinY <= bboxSecondObjMaxY ||
+          bboxFirstObjMinY <= bboxSecondObjMinY && bboxFirstObjMinY >= bboxSecondObjMaxY ||
+          bboxFirstObjMaxY >= bboxSecondObjMinY && bboxFirstObjMaxY <= bboxSecondObjMaxY ||
+          bboxFirstObjMaxY <= bboxSecondObjMinY && bboxFirstObjMaxY >= bboxSecondObjMaxY))
     {
       return false;
     }
